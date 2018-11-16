7b48f4114a7c25d91e15f1e78ed2a6b9ebf65bf2
ZGY <zgyarphetis@gmail.com>
2018-11-16 15:56:57 -0500

    Stack

--- CP1/W1-Balanced Brackets.cpp
+++ CP1/W1-Balanced Brackets.cpp
@@ -71,7 +71,7 @@ string isBalanced(string s) {
 int main(){
     ofstream fout(getenv("OUTPUT_PATH"));
     int t;
-    cin >> t;
+    cin>>t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     for (int t_itr = 0; t_itr < t; t_itr++) {
         string s;
