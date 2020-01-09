8ba0aa262fa201a5d665f44f0a7dee1703dcb266
RozeBusher <zgyarphetis@gmail.com>
2020-01-09 13:55:57 -0500

    Update DSU.cpp

--- Template/DSU.cpp
+++ Template/DSU.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAXN 1005
-int P[MAXN], R[MAXN];   //parent, rank
+int P[MAXN], R[MAXN];   //parent, rank(tree height)
 void init(){
   for(int i=0;i<MAXN;++i) P[i]=-(R[i]=1);
 }
+//path compression
 int find(int a){
   int p=a, t;
   for(;P[p]!=p;p=P[p]);
-  for(a!=p;a=t) t=P[a], P[a]=p;
+  for(;a!=p;a=t) t=P[a], P[a]=p;
   return a;
 }
+//union by rank
 void union(int a, int b){
   a=find(a), b=find(b);
   if(a==b) return;
