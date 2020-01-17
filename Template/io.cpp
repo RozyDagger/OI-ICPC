24a7ecc6e52c2e802217b4b871d77c8373a3507d
RozyDagger <zgyarphetis@gmail.com>
2020-01-16 23:59:50 -0500

    Update io.cpp

--- Template/io.cpp
+++ Template/io.cpp
@@ -1,17 +1,31 @@
-#include <cstdio>
-int readint(){
-  bool ng=0;
-  int c=getchar(), num=0;
-  c=getchar();
-  ng=c=='-';
-  for(c=getchar();c>47&&c<58;c=getchar()) num=num*10+c-48;
-  return ng?(~(num)+1):num;
+#include <stdio.h>
+#define M10(a) (((a)<<3)+((a)<<1))
+inline void readint(int &x){
+    int c=getchar_unlocked();
+    x=0;
+    bool ng=0;
+    for(;(c<48||c>57)&&c!='-';c=getchar_unlocked());
+    if(c=='-'){
+        ng=1;
+        c=getchar_unlocked();
+    }
+    for(;c>47&&c<58;c=getchar_unlocked()) x=M10(x)+c-48;
+    if(ng) x=-x;
 }
-void readstr(string &str){
-  int c, vi[64]={0}, len=0;
-  c=getchar();
-  for(;(c>47&&c<58)||(c>64&&c<91)||(c>96&&c<123);c=getchar()) vi[len++]=c;
-  //c=getchar();
-  str.resize(len);
-  for(int i=0;i<len;++i) str[i]=vi[i];
+inline void readstr(char *x){
+    char c=getchar_unlocked();
+    short i=0;
+    for(;c<97||c>122;c=getchar_unlocked());
+    for(;c>96&&c<123;c=getchar_unlocked()) x[i++]=c;
+}
+inline void writeint(int x){
+    int n=x, t=n, cnt=0;
+    if(!n){
+        putchar_unlocked('0');
+        return;
+    }
+    for(;!(t%10);t/=10) ++cnt;
+    for(t=0;n;n/=10) t=M10(t)+n%10;
+    for(;t;t/=10) putchar_unlocked(t%10+'0');
+    for(;cnt--;putchar_unlocked('0'));
 }
